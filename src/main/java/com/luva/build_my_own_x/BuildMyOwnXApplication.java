2eb244ac48142d32b9709a6f5388b3311b5a4046
Luva Hulva.H <lvsweet217@163.com>
2023-03-25 13:25:57 +0800

    init build my own x

---
+++ src/main/java/com/luva/build_my_own_x/BuildMyOwnXApplication.java
@@ -0,0 +1,13 @@
+package com.luva.build_my_own_x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildMyOwnXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildMyOwnXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luva/build_my_own_x/BuildMyOwnXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luva.build_my_own_x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildMyOwnXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
